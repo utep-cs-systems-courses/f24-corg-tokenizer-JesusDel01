773b7f5042a1ad4242763661cd8169e5f8991e62
JesusDel01 <jesusdelgado@Jesuss-MacBook-Pro-2.local>
2024-10-10 14:36:26 -0600

    improved uimain file

--- src/uimain.c
+++ src/uimain.c
@@ -1,34 +1,63 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 
 int main() {
-    char buffer[100];
-
-    while (1) {
+    unsigned char buffer_size = 100;
+    char *buffer = (char *)malloc(buffer_size * sizeof(char));
+    //make sure that memeory was allocated correctly
+    if (buffer == NULL){
+      printf("Failed to allocate memory\n");
+	goto out;
+    }
+    
+ top:
         printf("$ ");
         
         //read input using fgets
-        if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-	  buffer[strcspn(buffer, "\n")] = '\0'; // gets rid of the newline character
+        if (fgets(buffer, buffer_size, stdin) != NULL) {
+	 // Manually check if input is longer than the buffer size
+         unsigned int i = 0;
+         int is_full = 1;
+         for (i = 0; i < buffer_size - 1; i++) {
+            if (buffer[i] == '\n') {
+              is_full = 0;
+              buffer[i] = '\0';  // Replace newline with null terminator
+                 break;
+            }else if (buffer[i] == '\0') {
+              is_full = 0;
+                 break;
+	    }
+	 }
 
-            //check if "exit"
-            if (strcmp(buffer, "exit") == 0) {
-                break; //breaks the program
+	    if (is_full){
+	      char ch;
+	      while ((ch = getchar()) != '\n' && ch != EOF){
+		;//gets rid of the extra input that wont be tokenized
+	      }
+              printf("Warning: the input was too long, only the first 100 characters will be tokenized.\n");
             }
 
-	    char **tokens = tokenize(buffer);
-	    if(tokens != NULL){
-	      print_tokens(tokens);
-	      free_tokens(tokens);
+	  // manually check if 'exit'
+	  if(buffer[0] == 'e' && buffer[1] == 'x' &&buffer[2] == 'i' && buffer[3] == 't' && buffer[4] == '\0'){
+	    goto out;
+	  }
+
+	  //tokenize the input
+	   char **tokens = tokenize(buffer);
+	   if(tokens != NULL){
+	     print_tokens(tokens);
+	     free_tokens(tokens);
 		
-	    }
+	   }
 
         } else {
             printf("Error reading input.\n");
-            break;
+            goto out;
         }
-    }
-
+	goto top;
+ out:
+    //free the memory allocated by the buffer
+    free(buffer);
     return 0;
 }
